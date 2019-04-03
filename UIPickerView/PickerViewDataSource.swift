ec0c9768b56adb2977fd7aab4b18119927e2bd0a
yukimura03 <43407212+yukimura03@users.noreply.github.com>
2019-04-03 12:23:15 +0900

    選択肢が？になる

---
+++ UIPickerView/PickerViewDataSource.swift
@@ -0,0 +1,32 @@
+//
+//  PickerViewDataSource.swift
+//  UIPickerView
+//
+//  Created by arisa03g on 2019/04/03.
+//  Copyright © 2019 minagi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PickerViewDataSource: NSObject, UIPickerViewDataSource {
+    let array = ["赤","青","黄","緑"]
+    var color = ""
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return array.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return array[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        color = array[row]
+    }
+    
+}
--- UIPickerView/ViewController.swift
+++ UIPickerView/ViewController.swift
@@ -8,68 +8,68 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+final class ViewController: UIViewController, UIPickerViewDelegate {
+    
+    @IBOutlet weak private var label: UILabel!
+    @IBOutlet weak private var cancelButton: UIButton!
+    @IBOutlet weak private var doneButton: UIButton!
+    @IBOutlet weak var picker: UIPickerView!
+    let dataSource = PickerViewDataSource()
+    
+    //let array = ["赤","青","黄","緑"]
+    //var color = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         picker.isHidden = true
-        button_cancel.isHidden = true
-        button_done.isHidden = true
+        cancelButton.isHidden = true
+        doneButton.isHidden = true
         
         picker.delegate = self
-        picker.dataSource = self
+        picker.dataSource = dataSource
+        picker.selectRow(0, inComponent: 0, animated: true)
     }
-    
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var button_cancel: UIButton!
-    @IBOutlet weak var button_done: UIButton!
-    
+    /*
+     func numberOfComponents(in pickerView: UIPickerView) -> Int {
+     return 1
+     }
+     
+     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+     return array.count
+     }
+     
+     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+     return array[row]
+     }
+     
+     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+     color = array[row]
+     }
+     */
+    @IBAction private func tapLabel(_ sender: UILabel) {
+        picker.isHidden = false
+        cancelButton.isHidden = false
+        doneButton.isHidden = false
+    }
     
 
-    @IBAction func tapLabel(_ sender: Any) {
-        picker.isHidden = false
-        button_cancel.isHidden = false
-        button_done.isHidden = false
-    }
-    
-    let array = ["赤","青","黄","緑"]
-    var color = ""
-    
-    @IBOutlet weak var picker: UIPickerView!
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return array.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return array[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        color = array[row]
-    }
-    
-    
-    @IBAction func cancel(_ sender: Any) {
+    @IBAction private func tapCancelButton(_ sender: UIButton) {
         picker.isHidden = true
     }
-    
-    @IBAction func done(_ sender: Any) {
-        if color == "赤" {
-            view.backgroundColor = UIColor.red
-        } else if color == "青" {
-            view.backgroundColor = UIColor.blue
-        } else if color == "黄" {
-            view.backgroundColor = UIColor.yellow
-        } else if color == "緑" {
-            view.backgroundColor = UIColor.green
+
+    @IBAction private func tapDoneButton(_ sender: UIButton) {
+        switch dataSource.color {
+        case "赤":
+            view.backgroundColor = .red
+        case "青":
+            view.backgroundColor = .blue
+        case "黄":
+            view.backgroundColor = .yellow
+        case "緑":
+            view.backgroundColor = .green
+        default:
+            fatalError()
         }
         picker.isHidden = true
     }
-    
-    
 }
